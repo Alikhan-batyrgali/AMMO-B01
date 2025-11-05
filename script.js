6b0d28a333581c63f7d2c4862f24ee5720893777
Alikhan-batyrgali <162982866+Alikhan-batyrgali@users.noreply.github.com>
2025-11-06 02:23:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -113,6 +113,13 @@ function renderResults(data) {
     });
 }
 
+document.addEventListener('mousemove', (e) => {
+    const x = (e.clientX / window.innerWidth) * 100 + '%';
+    const y = (e.clientY / window.innerHeight) * 100 + '%';
+    document.body.style.setProperty('--mouse-x', x);
+    document.body.style.setProperty('--mouse-y', y);
+});
+
 // ---- Главный код ----
 // Ждем загрузки DOM перед инициализацией.
 document.addEventListener('DOMContentLoaded', () => {
